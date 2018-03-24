c71e7087113d19e01554632f61b37111814e0fea
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-24 16:06:00 -0400

    magic functions __name__ and __main__

---
+++ name_and_main/one.py
@@ -0,0 +1,9 @@
+def func():
+    print('Function 1')
+
+print("TOP level 1.py")
+
+if __name__ == '__main__':
+    print('1.py is being run directly!')
+else:
+    print("1.py has been imported")    
---
+++ name_and_main/two.py
@@ -0,0 +1,10 @@
+import one
+print("Top lvel in 2.py")
+
+one.func()
+
+
+if __name__ == '__main__':
+    print("2.py is being run directly")
+else:
+    print("2.py has been imported")
